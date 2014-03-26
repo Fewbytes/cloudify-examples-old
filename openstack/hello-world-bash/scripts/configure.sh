cf3f0b97f5b97a3b76672bd63ad8e059542232d8
iliapolo <elip@gigaspaces.com>
2014-03-26 17:01:54 +0200

    CFY-430 make blueprint openstack specific and not portable

--- openstack/hello-world-bash/scripts/configure.sh
+++ openstack/hello-world-bash/scripts/configure.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+. ${CLOUDIFY_LOGGING}
+
 TEMP_DIR="/tmp"
 PYTHON_FILE_SERVER_ROOT=${TEMP_DIR}/python-simple-http-webserver
+BLUEPRINT_PATH=blueprints/${CLOUDIFY_BLUEPRINT_ID}
 if [ -d ${PYTHON_FILE_SERVER_ROOT} ]; then
 	echo "Removing file server root folder ${PYTHON_FILE_SERVER_ROOT}"
 	rm -rf ${PYTHON_FILE_SERVER_ROOT} || exit $?
 fi
-echo "Creating HTTP server root directory at ${PYTHON_FILE_SERVER_ROOT}"
+info "Creating HTTP server root directory at ${PYTHON_FILE_SERVER_ROOT}"
 mkdir -p ${PYTHON_FILE_SERVER_ROOT} || exit $?
 
-cd ${PYTHON_FILE_SERVER_ROOT} || exit $?
-echo "Downloading index to web server"
+echo "Changing directory to ${PYTHON_FILE_SERVER_ROOT}"
+cd ${PYTHON_FILE_SERVER_ROOT}
+info "Downloading index to web server"
 wget -x -nH ${CLOUDIFY_FILE_SERVER_BLUEPRINT_ROOT}/${index_path} || exit $?
-echo "Downloading image to web server"
+info "Downloading image to web server"
 wget -x -nH ${CLOUDIFY_FILE_SERVER_BLUEPRINT_ROOT}/${image_path} || exit $?
 
 # Add dynamic data
 
 echo "Generating dynamic data"
 
-sed -i "s|{0}|${CLOUDIFY_BLUEPRINT_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${index_path} || exit $?
-sed -i "s|{1}|${CLOUDIFY_DEPLOYMENT_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${index_path} || exit $?
-sed -i "s|{2}|${CLOUDIFY_NODE_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${index_path} || exit $?
-sed -i "s|{3}|{image_path}|g" ${PYTHON_FILE_SERVER_ROOT}/${index_path} || exit $?
+sed -i "s|{0}|${CLOUDIFY_BLUEPRINT_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${BLUEPRINT_PATH}/${index_path} || exit $?
+sed -i "s|{1}|${CLOUDIFY_DEPLOYMENT_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${BLUEPRINT_PATH}/${index_path} || exit $?
+sed -i "s|{2}|${CLOUDIFY_NODE_ID}|g" ${PYTHON_FILE_SERVER_ROOT}/${BLUEPRINT_PATH}/${index_path} || exit $?
+sed -i "s|{3}|/blueprints/${CLOUDIFY_BLUEPRINT_ID}/{image_path}|g" ${PYTHON_FILE_SERVER_ROOT}/${BLUEPRINT_PATH}/${index_path} || exit $?
 
 echo "index file is ready"
 
--- openstack/hello-world-bash/scripts/start.sh
+++ openstack/hello-world-bash/scripts/start.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
+. ${CLOUDIFY_LOGGING}
+
 TEMP_DIR="/tmp"
 PYTHON_FILE_SERVER_ROOT=${TEMP_DIR}/python-simple-http-webserver
 PID_FILE="server.pid"
 
-echo "Starting HTTP server from ${PYTHON_FILE_SERVER_ROOT}"
+info "Starting HTTP server from ${PYTHON_FILE_SERVER_ROOT}"
 
+echo "Changing directory to ${PYTHON_FILE_SERVER_ROOT}"
 cd ${PYTHON_FILE_SERVER_ROOT} || exit $?
+info "Starting SimpleHTTPServer"
 nohup python -m SimpleHTTPServer ${port} > /dev/null 2>&1 &
 echo $! > ${PID_FILE}
 
-echo "Waiting for server to launch"
+info "Waiting for server to launch"
 
 STARTED=false
 for i in $(seq 1 15)
 do
 	if wget http://localhost:${port} 2>/dev/null ; then
-		echo "Server is up."
+		info "Server is up."
 		STARTED=true
     	break
 	else
-		echo "Server not up. waiting 1 second."
+		info "Server not up. waiting 1 second."
 		sleep 1
 	fi	
 done
 if [ ${STARTED} = false ]; then
-	echo "Failed starting web server in 15 seconds."
+	error "Failed starting web server in 15 seconds."
 	exit 1
 fi
